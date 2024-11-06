a9fe4e83d891ae10082f8965c91768ef143ed942
prdimitrov <prdimitrov97@gmail.com>
2024-11-06 09:08:26 +0200

    - Add constraints in the activity_main.xml - Add text colors - Add image in drawable folder - Add background image to the ConstraintLayout.

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.unitconverter;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    EditText editText;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +24,23 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            textView = findViewById(R.id.textView);
+            editText = findViewById(R.id.editText);
+            button = findViewById(R.id.button);
+
+            button.setOnClickListener(v1 -> {
+                try {
+                    double input = Double.parseDouble(editText.getText().toString());
+                    textView.setText(String.valueOf(convertToPounds(input)));
+                } catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this, "Please enter a valid number", Toast.LENGTH_SHORT).show();
+                }
+            });
             return insets;
         });
+    }
+    private double convertToPounds(double kilograms) {
+        // 1 kilogram = 2.20462262 pounds
+        return kilograms * 2.20462262;
     }
 }
